6dfad71da77fdc3e0d69a7f2f828e1b694ab6a36
yyachi <yusuke.yachi@gmail.com>
2019-01-31 15:22:21 +0900

    add script to save pmlame as RDS file

---
+++ script/get_pmlame.r
@@ -0,0 +1,15 @@
+library(MedusaRClient)
+library(yaml)
+
+initial.options <- commandArgs(trailingOnly = FALSE)
+file.arg.name <- "--file="
+script.name <- sub(file.arg.name,"",initial.options[grep(file.arg.name, initial.options)])
+script.basename <- dirname(script.name)
+config_path <- file.path(script.basename,"../config/medusa.yaml")
+config <- yaml.load_file(config_path)
+rds_path <- config$RDS
+
+json_path <- commandArgs(trailingOnly=TRUE)[1]
+obj <- Pmlame$new()
+df <- obj$read_local(json_path)
+saveRDS(df,rds_path)
